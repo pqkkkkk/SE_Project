9331744b3ffe6d571dfb212b6053fa7d4677e58f
kpq854 <crkeo169@gmail.com>
2024-11-10 16:33:43 +0700

    add project

---
+++ back-end/src/main/java/com/introduce2se/seproject/SeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.introduce2se.seproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeprojectApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/introduce2se/seproject/SeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introduce2se.seproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
