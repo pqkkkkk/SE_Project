d07850655d263012dd414228ab653e9bc2b8bbdc
Duong Khanh <22120155@student.hcmus.edu.vn>
2024-11-23 11:28:33 +0700

    detail doctor page

--- front-end/src/assets/images/index.js
+++ front-end/src/assets/images/index.js
@@ -1,9 +1,19 @@
 const images = {
-    logo: require('./logo.svg').default,
-    medicare: require('./Medicare.svg').default,
-    call: require('./call.svg').default,
-    doctorDefault: require('./doctor1.svg').default,
-}
-
+  logo: require("./logo.svg").default,
+  medicare: require("./Medicare.svg").default,
+  call: require("./call.svg").default,
+  doctorDefault: require("./doctor1.svg").default,
+  doctorImage: require("./doctorImage.svg").default,
+  star: require("./star.svg").default,
+  calendar: require("./calendar.svg").default,
+  phone: require("./phone.svg").default,
+  message: require("./message.svg").default,
+  smile: require("./smile.svg").default,
+  ear: require("./ear.svg").default,
+  location: require("./location.svg").default,
+  doctorImage2: require("./doctorImage2.svg").default,
+  doctorImage3: require("./doctorImage3.svg").default,
+  expandless: require("./expandless.svg").default,
+};
 
 export default images;
--- front-end/src/pages/FindDoctor/index.js
+++ front-end/src/pages/FindDoctor/index.js
@@ -1,115 +1,117 @@
-import classNames from 'classnames/bind';
-import styles from './FindDoctor.module.scss';
-import images from '../../assets/images';
+import classNames from "classnames/bind";
+import styles from "./FindDoctor.module.scss";
+import images from "../../assets/images";
+import { useNavigate } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
-function FindDoctor(){
-    return(
-        <div className={cx('content')}>
-            <h2 className={cx('desc')}>Search for Doctor , Make an Appointment</h2>
-            <div className={cx('border')}></div>
-            <div className={cx('page')}>
-                <div className={cx('sidebar')}>
-                    <div className={cx('search', 'element')}>
-                        <label className={cx('field-text')}>Search</label>
-                        <div className={cx('decor-input')}>
-                            <input className={cx('input')} type="text" placeholder="Search doctor" />
-                            <button className={cx('search-btn')}>
-                                Search
-                            </button>
-                        </div>
-                    </div>
-
-                    <form className={cx('filter', 'element')} id="filter-form">
-                        <div className={cx('gender-options')}>
-                            <label className={cx('field-text')}>Gender of doctor</label>
-                            <div className={cx('decor-gender')}>
-                                <label>
-                                    <input type="radio" name="gender" value="male" />
-                                    Male
-                                </label>
-                                <label>
-                                    <input type="radio" name="gender" value="female" />
-                                    Female
-                                </label>
-                            </div>
-                        </div>
-
-                        <div className={cx('speciality-options')}>
-                            <label htmlFor="speciality" className={cx('field-text')}>Speciality</label>
-                            <select id="speciality" className={cx('select-options')} name="speciality">
-                                <option value="" >All</option>
-                                <option value="cardiologist">Cardiologist</option>
-                                <option value="dentist">Dentist</option>
-                                <option value="dermatologist">Dermatologist</option>
-                                <option value="neurologist">Neurologist</option>
-                                <option value="psychiatrist">Psychiatrist</option>
-                            </select>
-                        </div>
-
-                        <div className={cx('experience-options')}>
-                            <label htmlFor="experience" className={cx('field-text')}>Experience</label>
-                            <select id="experience" className={cx('select-options')} name="experience">
-                                <option value="" >All</option>
-                                <option value="1">1 year</option>
-                                <option value="2">2 years</option>
-                                <option value="3">3 years</option>
-                                <option value="4">4 years</option>
-                                <option value="5">5 years</option>
-                                <option value="6+">6+ years</option>
-                            </select>
-                        </div>
-
-                        <button type="submit" className={cx('submit-btn')}>Show Doctors</button>
-                    </form>
-    
-                </div>
-                <div className={cx('main-content')}>
-                    <div className={cx('list-doctors')}>
-                        <div className={cx('doctor')}>
-                            <img src={images.doctorDefault} alt="doctor"/>
-                            <h3>Dr. John Doe</h3>
-                            <p className={cx('speciality')}>Cardiologist</p>
-                            <p className={cx('review')}>120 reviews</p>
-                        </div>
-                        <div className={cx('doctor')}>
-                            <img src={images.doctorDefault} alt="doctor"/>
-                            <h3>Dr. John Doe</h3>
-                            <p className={cx('speciality')}>Cardiologist</p>
-                            <p className={cx('review')}>120 reviews</p>
-                        </div>
-                        <div className={cx('doctor')}>
-                            <img src={images.doctorDefault} alt="doctor"/>
-                            <h3>Dr. John Doe</h3>
-                            <p className={cx('speciality')}>Cardiologist</p>
-                            <p className={cx('review')}>120 reviews</p>
-                        </div>
-                        <div className={cx('doctor')}>
-                            <img src={images.doctorDefault} alt="doctor"/>
-                            <h3>Dr. John Doe</h3>
-                            <p className={cx('speciality')}>Cardiologist</p>
-                            <p className={cx('review')}>120 reviews</p>
-                        </div>
-                        <div className={cx('doctor')}>
-                            <img src={images.doctorDefault} alt="doctor"/>
-                            <h3>Dr. John Doe</h3>
-                            <p className={cx('speciality')}>Cardiologist</p>
-                            <p className={cx('review')}>120 reviews</p>
-                        </div>
-                        <div className={cx('doctor')}>
-                            <img src={images.doctorDefault} alt="doctor"/>
-                            <h3>Dr. John Doe</h3>
-                            <p className={cx('speciality')}>Cardiologist</p>
-                            <p className={cx('review')}>120 reviews</p>
-                        </div>
-                        
-                    </div>
-                </div>
+function FindDoctor() {
+  const navigate = useNavigate();
+  const doctors = [
+    { id: 1, name: "Dr. John Doe 1", speciality: "Cardiologist", review: 120 },
+    { id: 2, name: "Dr. Jane Doe 2", speciality: "Dentist", review: 100 },
+    { id: 3, name: "Dr. John Doe 3", speciality: "Dermatologist", review: 80 },
+    { id: 4, name: "Dr. John Doe 4", speciality: "Neurologist", review: 90 },
+    { id: 5, name: "Dr. John Doe 5", speciality: "Psychiatrist", review: 110 },
+    { id: 6, name: "Dr. John Doe 6", speciality: "Cardiologist", review: 120 },
+  ];
+  const handleDoctorClick = (id) => {
+    navigate(`/doctor/${id}`);
+  };
+  return (
+    <div className={cx("content")}>
+      <h2 className={cx("desc")}>Search for Doctor , Make an Appointment</h2>
+      <div className={cx("border")}></div>
+      <div className={cx("page")}>
+        <div className={cx("sidebar")}>
+          <div className={cx("search", "element")}>
+            <label className={cx("field-text")}>Search</label>
+            <div className={cx("decor-input")}>
+              <input
+                className={cx("input")}
+                type="text"
+                placeholder="Search doctor"
+              />
+              <button className={cx("search-btn")}>Search</button>
             </div>
+          </div>
+
+          <form className={cx("filter", "element")} id="filter-form">
+            <div className={cx("gender-options")}>
+              <label className={cx("field-text")}>Gender of doctor</label>
+              <div className={cx("decor-gender")}>
+                <label>
+                  <input type="radio" name="gender" value="male" />
+                  Male
+                </label>
+                <label>
+                  <input type="radio" name="gender" value="female" />
+                  Female
+                </label>
+              </div>
+            </div>
+
+            <div className={cx("speciality-options")}>
+              <label htmlFor="speciality" className={cx("field-text")}>
+                Speciality
+              </label>
+              <select
+                id="speciality"
+                className={cx("select-options")}
+                name="speciality"
+              >
+                <option value="">All</option>
+                <option value="cardiologist">Cardiologist</option>
+                <option value="dentist">Dentist</option>
+                <option value="dermatologist">Dermatologist</option>
+                <option value="neurologist">Neurologist</option>
+                <option value="psychiatrist">Psychiatrist</option>
+              </select>
+            </div>
+
+            <div className={cx("experience-options")}>
+              <label htmlFor="experience" className={cx("field-text")}>
+                Experience
+              </label>
+              <select
+                id="experience"
+                className={cx("select-options")}
+                name="experience"
+              >
+                <option value="">All</option>
+                <option value="1">1 year</option>
+                <option value="2">2 years</option>
+                <option value="3">3 years</option>
+                <option value="4">4 years</option>
+                <option value="5">5 years</option>
+                <option value="6+">6+ years</option>
+              </select>
+            </div>
+
+            <button type="submit" className={cx("submit-btn")}>
+              Show Doctors
+            </button>
+          </form>
         </div>
-        
-    )
+        <div className={cx("main-content")}>
+          <div className={cx("list-doctors")}>
+            {doctors.map((doctor) => (
+              <div
+                key={doctor.id}
+                className={cx("doctor")}
+                onClick={() => handleDoctorClick(doctor.id)}
+              >
+                <img src={images.doctorDefault} alt="doctor" />
+                <h3>{doctor.name}</h3>
+                <p className={cx("speciality")}>{doctor.speciality}</p>
+                <p className={cx("review")}>{doctor.reviews} reviews</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FindDoctor;
